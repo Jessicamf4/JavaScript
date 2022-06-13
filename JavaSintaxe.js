ff725471ec6d19e4419037b4e0c07f101d5e06ae
Jessicamf4 <jessicamarquesfialho0@gmail.com>
2022-06-13 16:52:42 -0300

    Veriáveis e sintaxe

---
+++ JavaSintaxe.js
@@ -0,0 +1,24 @@
+const number1 = 4
+const number2 = 7
+
+if (number1 === number2 ){
+    console.log(number1, " é igual a", number2);
+}
+else {
+    console.log(number1, "é diferente de ", number2);
+}
+
+if (number1 + number2 > 10){
+    console.log(number1 + number2, " é maior que 10")
+}
+else {
+    console.log(number1 + number2, " é menor que 10")
+}
+
+if (number1 + number2 > 20){
+    console.log(number1 + number2, " é maior que 20")
+}
+else {
+    console.log(number1 + number2, " é menor que 20")
+}
+
